fbce625960bf824b93e79050678055b39b2df23d
DanielSzpunar <danielszpunar@gmail.com>
2017-08-31 21:51:14 -0400

    wk1ForLoops.py

---
+++ MITx6001/wk1ForLoops.py
@@ -0,0 +1,9 @@
+#Prints 0 to 99
+for x in range(100):
+    print(x)
+#Prints 50 to 100
+for y in range(50, 100):
+    print(y)
+#Prints 1 to 100 in iterations of 3
+for z in range(1, 100, 3):
+    print(z)
