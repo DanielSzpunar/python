f16c3fd9db2a80acd77df4b53bfd55cfabda9552
DanielSzpunar <danielszpunar@gmail.com>
2017-09-03 21:47:33 -0400

    MITx6001

---
+++ MITx6001/wk1_exercise_counting_vowels.py
@@ -0,0 +1,6 @@
+string = "string"
+totalVowels = 0
+for f in string:
+    if f == "a" or f == "e" or  f == "i" or f == "o" or  f == "u":
+        totalVowels += 1
+print("the total amount of vowels in the string entered are:", totalVowels)
